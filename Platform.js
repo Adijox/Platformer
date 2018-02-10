83c6ed39a9b1b67b035c40ff3dc2d6538b8450e7
ntnbr <antoineaubry1002@gmail.com>
2018-02-10 17:58:56 +0100

    nothing new sub sole

--- Platform.js
+++ Platform.js
@@ -10,8 +10,8 @@ function Platform(x,y,width,col,index) {
 //Afficher la plateforme
   this.display = function() {
     fill(this.col);
-//20:valeur de la courbure des angles
-//10:largeur arbitraire
+//30:valeur de la courbure des angles
+//7:largeur arbitraire
     rect(this.x, this.y, this.width, this.height, 30);
   }
 }
